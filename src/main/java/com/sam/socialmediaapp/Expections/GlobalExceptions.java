fd62c46b025d6c6bd1ed0b224d82bea3eb098aae
Sankalp Mehta <1js23cs150@jssateb.ac.in>
2026-03-23 02:28:01 +0530

    Exceptions changes

--- src/main/java/com/sam/socialmediaapp/Expections/GlobalExceptions.java
+++ src/main/java/com/sam/socialmediaapp/Expections/GlobalExceptions.java
@@ -2,7 +2,6 @@ package com.sam.socialmediaapp.Expections;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
